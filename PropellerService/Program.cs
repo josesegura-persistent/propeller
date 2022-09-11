a485ceae11738450763e1fd3494112b0514aba9e
Jose Luis Segura <jose_segura@persistent.com>
2022-09-11 16:37:29 -0500

    Changes dockerfile location

--- PropellerService/Program.cs
+++ PropellerService/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddRouting(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
